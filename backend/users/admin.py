4501bbcec14ade4c9121c90905b659217f2576b1
Mariia Samokhina <mshono@yandex.ru>
2024-10-01 12:40:00 +0300

    0.4 Added the admin part to CustomUser model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "avatar",
+    )
+    list_filter = ("username",)
+    search_fields = (
+        "username",
+    )
