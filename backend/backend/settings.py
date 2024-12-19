6de12a2d02326264dcb36de1a2d2ab71025e19be
Mariia Samokhina <mshono@yandex.ru>
2024-12-20 00:33:16 +0200

    5.3 Added logging to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,6 +60,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 DATABASES = {
     "default": {
