c6344ad52634f5932281439523b7e1e03c1b1fc9
Mariia Samokhina <mshono@yandex.ru>
2024-12-27 12:09:23 +0200

    6.1 Made changes in admin.py

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -7,6 +7,7 @@ from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ("pk", "name", "slug")
+    list_display_links = ("name",)
     search_fields = ("name",)
     empty_value_display = "-пусто-"
 
@@ -21,14 +22,23 @@ class IngredientAdmin(admin.ModelAdmin):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("pk", "author", "name")
-    search_fields = ("name", "author")
-    list_filter = ("tags",)
+    list_display_links = ("author",)
+    list_filter = ("name", "author", "tags",)
     empty_value_display = "-пусто-"
     readonly_fields = ("favorites_count",)
 
+    def get_queryset(self, request):
+        """Оптимизация запроса для списка рецептов."""
+        queryset = super().get_queryset(request)
+        return queryset.select_related(
+            "author"
+        ).prefetch_related(
+            "tags", "ingredients"
+        )
+
     @admin.display(description="Количество в избранном")
     def favorites_count(self, obj):
-        """Returnes the ammount of adding the recipe to the favorites"""
+        """Возвращает количество рецептов, добавленных в избранное"""
         return obj.favorites.count()
 
 
@@ -37,6 +47,7 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = (
         "pk", "recipe", "ingredients", "amount",
     )
+    list_display_links = ("recipe",)
     search_fields = ("recipie",)
     empty_value_display = "-пусто-"
 
@@ -44,12 +55,24 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = ("pk", "recipe", "user")
+    list_display_links = ("recipe",)
     search_fields = ("recipe",)
     empty_value_display = "-пусто-"
+
+    def get_queryset(self, request):
+        """Оптимизация запроса для избранного."""
+        queryset = super().get_queryset(request)
+        return queryset.select_related("user")
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ("pk", "recipe", "user")
+    list_display_links = ("recipe",)
     search_fields = ("recipe",)
     empty_value_display = "-пусто-"
+
+    def get_queryset(self, request):
+        """Оптимизация запроса для корзины покупок."""
+        queryset = super().get_queryset(request)
+        return queryset.select_related("user")
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,22 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models import Value
+from django.db.models.functions import Concat
 
 from .models import Subscription, User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = (
         "username",
         "email",
         "first_name",
         "last_name",
         "avatar",
+        'is_superuser',
+        'is_active',
     )
-    list_filter = ("username",)
     search_fields = (
-        "username", "email"
+        "username", "email", "first_name", "last_name",
     )
+
+    def get_search_results(self, request, queryset, search_term):
+        """Поиск для first_name и last_name в одном запросе."""
+
+        queryset, use_distinct = super().get_search_results(
+            request, queryset, search_term
+        )
+        if search_term:
+            full_name_query = queryset.annotate(
+                full_name=Concat(
+                    'first_name', Value(' '), 'last_name'
+                )
+            )
+            queryset |= full_name_query.filter(
+                full_name__icontains=search_term
+            )
+        return queryset, use_distinct
 
 
 @admin.register(Subscription)
